dcfcb39178f127305d41bfb0d16fa9fe7e079e8c
9631kunn <infourbh+1@gmail.com>
2021-01-08 12:52:00 +0900

    add: app.js page/index.ejs

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log("STARTED: " + PORT));
