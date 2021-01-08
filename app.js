3601f8c3b4ddcc963c7e33c054677d3855b030e1
9631kunn <infourbh+1@gmail.com>
2021-01-08 13:32:19 +0900

    add: login functionally

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const env = process.env;
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = env.PORT || 3000;
+const querystring = require("querystring");
 
 app
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
   .get("/", (req, res) => res.render("pages/index"))
+  .get("/login", (req, res) => {
+    const query = querystring.stringify({
+      response_type: "code",
+      client_id: env.LINE_CHANNEL_ID,
+      redirect_uri: env.URL + "/callback",
+      state: "hoge",
+      scope: "profile",
+    });
+    res.redirect(301, "https://access.line.me/oauth2/v2.1/authorize?" + query);
+  })
+  .get("/callback", (req, res) => {
+    res.send("code: " + req.query.code);
+  })
   .listen(PORT, () => console.log("STARTED: " + PORT));
